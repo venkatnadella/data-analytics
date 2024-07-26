5af8f463157cd1456c3d56a368fbffbfcf9d26bf
venkatnadella <indolar23@gmail.com>
2024-07-26 00:14:33 -0400

    adding files from local repo after deploying

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
 from app import load_data, analyze_data
 
@@ -15,12 +9,9 @@ def test_load_data():
 def test_analyze_data():
     df = load_data('data/sample_data.csv')
     summary = analyze_data(df)
-    assert 'count' in summary.columns
-    assert 'mean' in summary.columns
-    assert 'std' in summary.columns
+    assert 'index' in summary.columns
+    assert 'value' in summary.columns
 
-
-# In[ ]:
 
 
 
